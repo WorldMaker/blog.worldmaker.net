18c8ec9b39f3493089889a3d716f62245dc9f858
Max Battcher <me@worldmaker.net>
2026-05-08 17:12:07 -0400

    🔥 Swith to Lume; posts from the DRAGONzine years

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
 import lume from "lume/mod.ts"
+import extractDate from 'lume/plugins/extract_date.ts'
 import feed from 'lume/plugins/feed.ts'
 import redirects from "lume/plugins/redirects.ts"
 
@@ -6,6 +7,8 @@ const site = lume()
 
 site.copy('assets')
 site.copy('favicon.ico')
+
+site.use(extractDate())
 
 site.use(feed({
     output: ['feed.xml', 'rss.xml', 'feed/latest/index.html', 'node/feed/index.html', 'feed.json'],
